a1c0b28da410d93255dbd0cd629d7474bf7143ad
Abdelhadi Oussous <dadi94230@hotmail.fr>
2022-10-26 18:47:04 +0200

    screen desktop and mobil and start vue js

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-const { createApp } = Vue
 
+const { createApp } = Vue
 tab = ['laise', 'full', 'va reviser le quran c bn sah']
+
+var body = document.querySelector('body');
+var elements = document.querySelectorAll('.content__pays');
+var input = document.querySelector('input');
 
 createApp({
   data() {
     return {
       message: 'Hello Vue!',
-      tabs : tab
+      tabs : tab,
+      info: null,
+      pays : null,
     }
+  },
+  methods : {
+
+  },
+  mounted(){
+    axios
+      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(response => (this.info = response))
+    axios.get('https://restcountries.com/v3.1/all')
+    .then(response => this.pays = response.data)
   }
 }).mount('body')
 
