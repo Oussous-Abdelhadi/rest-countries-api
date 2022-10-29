4c254c4ca3a96480c88097356d827b5c2f4c5dca
Abdelhadi Oussous <dadi94230@hotmail.fr>
2022-10-29 20:46:06 +0200

    add detail page

--- script.js
+++ script.js
@@ -10,7 +10,9 @@ createApp({
     return {
       message: 'Hello Vue!',
       pays : null,
-      modal : false,
+      pays_detail : null,
+      borders : [],
+      borders_d : [],
     }
   },
 
@@ -45,27 +47,55 @@ createApp({
         .then(response => this.pays = response.data.sort(
           (a, b) =>  a.name.common.localeCompare(b.name.common)
           ))
-
       }
     },
 
+    
     parseNumber : function (x) {
       return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     },
     
-    details_pays : function (e) {
-      console.log("my bad");
+    details_pays : function (pay) {
+      this.borders = [];
+      this.pays_detail = pay;
+      if (pay.borders) {
+        for (let index = 0; index < 3; index++) {
+            this.borders.push(pay.borders[index]);
+          };
+      }
+
+      this.borders_d = [];
+      this.borders.forEach(element => {
+        axios.get(`https://restcountries.com/v3.1/alpha/${element}`)
+        .then(response => this.borders_d.push(response.data[0].name.common))
+      });
+    },
+    show : function (e) {
+      // console.log(e);
+      var main = document.querySelector("main");
+      var detail = document.querySelector(".container__details");
+      var tab = [main, detail];
+      tab.forEach(element => {
+        if (element.classList.contains("hidden")) {
+          element.classList.remove("hidden");
+        }
+        else {
+          element.classList.add("hidden");
+        }
+        
+      });
+    },
+    
+    parseData : function (str) {
+      var n = str.toString().replace(/_/g, "");
+      return n;
     },
 
-    toggle() {
-      this.modal = !this.modal;
-      console.log("yes");
-    }
-
+    
   },
 
 
-  created(){
+  mounted(){
     axios.get('https://restcountries.com/v3.1/all')
     .then(response => this.pays = response.data.sort(
       (a, b) =>  a.name.common.localeCompare(b.name.common)
